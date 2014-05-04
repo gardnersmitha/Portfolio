7cb1e8d1c3cb7675c538e4d48f17af10ec9d7027
gardnersmitha <austingardnersmith@gmail.com>
2014-05-04 15:58:32 -0400

    adding script.js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$('#avatar').tooltip();
+	//alert('test');
+	console.log('testttt');
+});
+
+/*
+var avatarTooltip = function(){
+	$('#avatar').tooltip({
+		trigger: 'hover',
+		selector: 'a[rel=tooltip]'
+	});
+	console.log('finished tooltip')
+};
+*/
